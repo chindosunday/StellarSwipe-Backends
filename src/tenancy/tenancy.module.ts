5d75937b01560bd019e480f6a8303f22d508df88
Favour Sabo <sabofavour4@gmail.com>
2026-06-29 16:17:06 +0100

    feat: add custom TypeORM query builder helper enforcing tenant-scoped WHERE clauses (#774)

--- src/tenancy/tenancy.module.ts
+++ src/tenancy/tenancy.module.ts
@@ -3,6 +3,7 @@ import { TenantScopingService } from './tenant-scoping.service';
 import { TenantRlsSubscriber } from './tenant-rls.subscriber';
 import { TenantDataSourceFactory } from './tenant-connection.factory';
 import { TenantConnectionProvider } from './tenant-connection.provider';
+import { TenantScopedQueryHelper } from './helpers/tenant-scoped-query.helper';
 
 @Global()
 @Module({
@@ -11,12 +12,14 @@ import { TenantConnectionProvider } from './tenant-connection.provider';
     TenantRlsSubscriber,
     TenantDataSourceFactory,
     TenantConnectionProvider,
+    TenantScopedQueryHelper,
   ],
   exports: [
     TenantScopingService,
     TenantRlsSubscriber,
     TenantDataSourceFactory,
     TenantConnectionProvider,
+    TenantScopedQueryHelper,
   ],
 })
 export class TenancyModule {}
